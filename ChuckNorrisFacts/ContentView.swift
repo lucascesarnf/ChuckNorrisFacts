89a68a58839b2fd365505eb8fea8169641d50960
Lucas César <lucascesarnf@hotmail.com>
2019-10-21 20:56:05 -0300

    Project was created

---
+++ ChuckNorrisFacts/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChuckNorrisFacts
+//
+//  Created by Lucas César  Nogueira Fonseca on 21/10/19.
+//  Copyright © 2019 Lucas César  Nogueira Fonseca. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
